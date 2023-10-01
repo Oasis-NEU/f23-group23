6f3b9cc4b6217d5b24a1e3738d58f847a9d282b2
Alex Dong <81170256+alexanderdong8@users.noreply.github.com>
2023-10-01 14:01:17 -0400

    firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import Image from 'next/image'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVU2hgf1Yv8uEYftVLEZbgps9K2Pebghc",
+  authDomain: "peace-of-mind-c4135.firebaseapp.com",
+  projectId: "peace-of-mind-c4135",
+  storageBucket: "peace-of-mind-c4135.appspot.com",
+  messagingSenderId: "619407910041",
+  appId: "1:619407910041:web:2d4a85f85f8031e2628408",
+  measurementId: "G-6ZFCPF2RCY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
